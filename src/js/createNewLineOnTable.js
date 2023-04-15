6973e566079eee350f350ee80efb5eb852ebc29e
RianVitor26 <rianvitorlhe@gamil.com>
2023-04-15 03:09:12 -0300

    feat: adicionando icones pen e trash a adicionando função remover linha

--- src/js/createNewLineOnTable.js
+++ src/js/createNewLineOnTable.js
@@ -1,14 +1,35 @@
-
-
-
 export function createNewLineOnTable(tableID, inputValue) {
-    const cell = document.createElement('td')
-    cell.innerHTML = inputValue
+    const cell1 = document.createElement('td')
+    cell1.innerHTML = inputValue
+
+    const cell2 = document.createElement('td')
+    const editIcon = document.createElement('i')
+    editIcon.className = 'fa-solid fa-pen'
+    cell2.appendChild(editIcon)
+
+    const cell3 = document.createElement('td')
+    const removeIcon = document.createElement('i')
+    removeIcon.className = 'fa-solid fa-trash'
+    cell3.appendChild(removeIcon)
 
     const newLine = document.createElement('tr')
 
-    newLine.append(cell)
+    newLine.append(cell1)
+    newLine.append(cell2)
+    newLine.append(cell3)
 
     const table = document.getElementById(tableID)
     table.appendChild(newLine)
-}
+
+    removeIcon.addEventListener('click', () => {
+        //<i> --> <td> --> <tr>.remove()
+        const row = removeIcon.parentNode.parentNode;
+        row.remove()
+    })
+
+    editIcon.addEventListener('click', () => {
+        //<i> --> <td> --> <tr>.edit()
+        const row = editIcon.parentNode.parentNode;
+        row.edit()
+    })
+    }
